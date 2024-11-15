123a8528a0efae419038d566efcaefffecb1443d
faith <jamienibtong@gmail.com>
2024-11-15 21:52:51 +0800

    try

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        typewriter: "typewriter 2s steps(54, end) forwards",
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: "100%",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
